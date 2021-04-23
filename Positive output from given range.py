c09311311732fd5a8ae44cb464e9693d467581cd
Riddhijit Basu <74175950+riddhijitbasu@users.noreply.github.com>
2021-04-24 03:59:45 +0530

    Practicing loops

---
+++ Positive output from given range.py
@@ -0,0 +1,9 @@
+list1=[1,2,-56,89,-5,39]
+for x in list1:
+    if x>0:
+        print(x, end=" ")
+print("\n")
+list2=[14,212,-506,899,-5309,-39]
+for x in list2:
+    if x>0:
+        print(x, end=" ")
